3475db4fcb4a4a9deb5e2157e0650f9fb33bf078
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2016-12-08 20:53:43 +0200

    first save

---
+++ src/com/hillel/variables/Vars.java
@@ -0,0 +1,25 @@
+package com.hillel.variables;
+
+public class Vars {
+  public static void main(String[] args) {
+    //byte var = 127;
+    //int num = var + 1;
+
+    //byte anotherVar = (byte) num;
+    //System.out.println(anotherVar);
+
+    /*
+    another comment
+    */
+
+    float var = 3.5f;
+    double d = 34.45;
+
+    double a = 30;
+    double b = 25;
+
+    double c = a / b;
+
+    System.out.println(c);
+  }
+}
